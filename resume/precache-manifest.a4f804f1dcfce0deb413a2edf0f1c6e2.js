8a1df1b0f0987f8c0371212e513e08924a927ab8
dingkailiang <dingkailiang@gmail.com>
2019-10-12 22:00:25 -0600

    Update

---
+++ resume/precache-manifest.a4f804f1dcfce0deb413a2edf0f1c6e2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894443028919a6fcf0965c6c6849d660",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "0b4166e22e4e3441c346",
+    "url": "/resume/static/css/main.9d538224.chunk.css"
+  },
+  {
+    "revision": "1aabdfbd13464f761e3f",
+    "url": "/resume/static/js/2.b0e2fd6e.chunk.js"
+  },
+  {
+    "revision": "0b4166e22e4e3441c346",
+    "url": "/resume/static/js/main.ab3a27ba.chunk.js"
+  },
+  {
+    "revision": "8aa44a54b53daee68cce",
+    "url": "/resume/static/js/runtime-main.23e5e7aa.js"
+  },
+  {
+    "revision": "0f964a72f8fbdf9c8d4e0122b8effb40",
+    "url": "/resume/static/media/devicon.0f964a72.woff"
+  },
+  {
+    "revision": "29f80b00a7b4641183f96f08be374697",
+    "url": "/resume/static/media/devicon.29f80b00.ttf"
+  },
+  {
+    "revision": "681fa39a79c22f0035a0720e2b2bda3b",
+    "url": "/resume/static/media/devicon.681fa39a.eot"
+  },
+  {
+    "revision": "74e6e6c3dcfca56767adabe83a510f2e",
+    "url": "/resume/static/media/devicon.74e6e6c3.svg"
+  },
+  {
+    "revision": "4ae481e6cff83eda2757d1c2407f976b",
+    "url": "/resume/static/media/resume-bg.4ae481e6.jpg"
+  }
+]);
--- resume/service-worker.js
+++ resume/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.095c203c2f4c506d0a5f4239291c9a2a.js"
+  "/resume/precache-manifest.a4f804f1dcfce0deb413a2edf0f1c6e2.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/resume/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
